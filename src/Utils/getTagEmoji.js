1ddb62dfd03d8691fde08115fe17cfbecc563df2
Restyled.io <commits@restyled.io>
2021-10-05 01:46:03 +0000

    Restyled by prettier

--- src/Utils/getTagEmoji.js
+++ src/Utils/getTagEmoji.js
@@ -1,19 +1,19 @@
 const TAG_EMOJI = {
-  Tshirt : "👕",
-  Stickers : "🏷️",
-  Bottle : "🍾",
-  Socks : "🧦",
-  Hoodie : "🧥",
-  Cap : "🧢",
-  Mug : "🍺",
-  Voucher : "🎟",
-  Jacket : "🧥",
-  Backpack : "🎒",
-  Swags : "🛍️",
-  Swag : "🛍️",
+  Tshirt: "👕",
+  Stickers: "🏷️",
+  Bottle: "🍾",
+  Socks: "🧦",
+  Hoodie: "🧥",
+  Cap: "🧢",
+  Mug: "🍺",
+  Voucher: "🎟",
+  Jacket: "🧥",
+  Backpack: "🎒",
+  Swags: "🛍️",
+  Swag: "🛍️",
 };
 
 const DEFAULT_TAG_EMOJI = "💜";
 
 export const getTagEmoji = (tag) =>
-    TAG_EMOJI[tag] ? TAG_EMOJI[tag] : DEFAULT_TAG_EMOJI;
+  TAG_EMOJI[tag] ? TAG_EMOJI[tag] : DEFAULT_TAG_EMOJI;
