b65a24962f03d734618c0f352434baa8747b1094
Restyled.io <commits@restyled.io>
2022-10-01 12:42:07 +0000

    Restyled by prettier

--- src/Utils/getTagColor.js
+++ src/Utils/getTagColor.js
@@ -11,26 +11,26 @@ const LIGHT_ORANGE = "#FA7070";
 const BROWN_RED = "#CA965C";
 const MEDIUM_TURQUOISE = "#48D1CC";
 const LIGHT_YELLOW = "#F88F01";
-const DARK_KHAKI = "#BDB76B"
+const DARK_KHAKI = "#BDB76B";
 
 const TAG_COLORS = {
-  Tshirt : LIGHT_ROSE,
-  Stickers : BERLY_GREEN,
-  Socks : GAINSBORO,
-  Hoodie : LIGHT_SALMON,
-  Cap : PARCHMENT,
-  Swags : MISTY_ROSE,
-  Swag : MISTY_ROSE,
-  Notebook : MEDIUM_CREAM,
-  Pen : SUNSET_MID,
-  Mug : LIGHT_ORANGE,
-  Jacket : BROWN_RED,
-  Bag : MEDIUM_TURQUOISE,
-  Hat : LIGHT_YELLOW,
-  Bottle : DARK_KHAKI,
+  Tshirt: LIGHT_ROSE,
+  Stickers: BERLY_GREEN,
+  Socks: GAINSBORO,
+  Hoodie: LIGHT_SALMON,
+  Cap: PARCHMENT,
+  Swags: MISTY_ROSE,
+  Swag: MISTY_ROSE,
+  Notebook: MEDIUM_CREAM,
+  Pen: SUNSET_MID,
+  Mug: LIGHT_ORANGE,
+  Jacket: BROWN_RED,
+  Bag: MEDIUM_TURQUOISE,
+  Hat: LIGHT_YELLOW,
+  Bottle: DARK_KHAKI,
 };
 
 const DEFAULT_TAG_COLOR = MERCURY;
 
 export const getTagColor = (tag) =>
-    TAG_COLORS[tag] ? TAG_COLORS[tag] : DEFAULT_TAG_COLOR;
+  TAG_COLORS[tag] ? TAG_COLORS[tag] : DEFAULT_TAG_COLOR;
--- src/Utils/getTagEmoji.js
+++ src/Utils/getTagEmoji.js
@@ -1,25 +1,25 @@
 const TAG_EMOJI = {
-  Tshirt : "👕",
-  Stickers : "🏷️",
-  Bottle : "🍾",
-  Socks : "🧦",
-  Hoodie : "🧥",
-  Cap : "🧢",
-  Mug : "🍺",
-  Voucher : "🎟",
-  Jacket : "🧥",
-  Backpack : "🎒",
-  Bag : "🎒",
-  Swags : "🛍️",
-  Swag : "🛍️",
-  Notebook : "📔",
-  Pen : "🖊️",
-  Laptop : "💻",
-  Cash : "💵",
-  Hat : "🎓"
+  Tshirt: "👕",
+  Stickers: "🏷️",
+  Bottle: "🍾",
+  Socks: "🧦",
+  Hoodie: "🧥",
+  Cap: "🧢",
+  Mug: "🍺",
+  Voucher: "🎟",
+  Jacket: "🧥",
+  Backpack: "🎒",
+  Bag: "🎒",
+  Swags: "🛍️",
+  Swag: "🛍️",
+  Notebook: "📔",
+  Pen: "🖊️",
+  Laptop: "💻",
+  Cash: "💵",
+  Hat: "🎓",
 };
 
 const DEFAULT_TAG_EMOJI = "💜";
 
 export const getTagEmoji = (tag) =>
-    TAG_EMOJI[tag] ? TAG_EMOJI[tag] : DEFAULT_TAG_EMOJI;
+  TAG_EMOJI[tag] ? TAG_EMOJI[tag] : DEFAULT_TAG_EMOJI;
