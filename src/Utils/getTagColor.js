b8a3e85e4d450580a20db0f872304ea088e47441
Restyled.io <commits@restyled.io>
2022-10-01 12:42:05 +0000

    Restyled by clang-format

--- src/Utils/getTagColor.js
+++ src/Utils/getTagColor.js
@@ -6,28 +6,28 @@ const MERCURY = "#150485";
 const PARCHMENT = "#1E5F74";
 const MISTY_ROSE = "#363062";
 const MEDIUM_CREAM = "#FFB4B4";
-const SUNSET_MID= "#7FB77E";
-const LIGHT_ORANGE="#FA7070";
-const BROWN_RED ="#CA965C";
-const MEDIUM_TURQUOISE="#48D1CC";
-const LIGHT_YELLOW="#F88F01";
+const SUNSET_MID = "#7FB77E";
+const LIGHT_ORANGE = "#FA7070";
+const BROWN_RED = "#CA965C";
+const MEDIUM_TURQUOISE = "#48D1CC";
+const LIGHT_YELLOW = "#F88F01";
 const DARK_KHAKI = "#BDB76B"
 
 const TAG_COLORS = {
-    Tshirt: LIGHT_ROSE,
-    Stickers: BERLY_GREEN,
-    Socks: GAINSBORO,
-    Hoodie: LIGHT_SALMON,
-    Cap: PARCHMENT,
-    Swags: MISTY_ROSE,
-    Swag: MISTY_ROSE,
-    Notebook:MEDIUM_CREAM,
-    Pen:SUNSET_MID,
-    Mug:LIGHT_ORANGE,
-    Jacket:BROWN_RED,
-    Bag:MEDIUM_TURQUOISE,
-    Hat:LIGHT_YELLOW,
-    Bottle:DARK_KHAKI,
+  Tshirt : LIGHT_ROSE,
+  Stickers : BERLY_GREEN,
+  Socks : GAINSBORO,
+  Hoodie : LIGHT_SALMON,
+  Cap : PARCHMENT,
+  Swags : MISTY_ROSE,
+  Swag : MISTY_ROSE,
+  Notebook : MEDIUM_CREAM,
+  Pen : SUNSET_MID,
+  Mug : LIGHT_ORANGE,
+  Jacket : BROWN_RED,
+  Bag : MEDIUM_TURQUOISE,
+  Hat : LIGHT_YELLOW,
+  Bottle : DARK_KHAKI,
 };
 
 const DEFAULT_TAG_COLOR = MERCURY;
